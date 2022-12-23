d9246340d3f50e92325c9b2382d33d8130473eb0
Sergei Papikian <I_SER_I@hotmail.com>
2022-12-23 18:52:56 +0300

    feat: add CORds

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import * as fs from 'fs';
+import supertokens from 'supertokens-node';
 
 async function start() {
   const port = process.env.PORT || 3001;
@@ -17,21 +18,21 @@ async function start() {
     AppModule,
     new FastifyAdapter({ https: httpsOptions }),
   );
-  // app.enableCors({
-  //   origin: [
-  //     process.env.AUTH_WEBSITE_DOMAIN,
-  //     'http://' + process.env.WEBSITE_DOMAIN,
-  //     'http://' + process.env.WEBSITE_DOMAIN + ':80',
-  //     'http://' + process.env.WEBSITE_DOMAIN + ':3000',
-  //     'http://' + process.env.WEBSITE_DOMAIN + ':3001',
-  //     'https://' + process.env.WEBSITE_DOMAIN,
-  //     'https://' + process.env.WEBSITE_DOMAIN + ':80',
-  //     'https://' + process.env.WEBSITE_DOMAIN + ':3000',
-  //     'https://' + process.env.WEBSITE_DOMAIN + ':3001',
-  //   ],
-  //   allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: [
+      process.env.AUTH_WEBSITE_DOMAIN,
+      'http://' + process.env.WEBSITE_DOMAIN,
+      'http://' + process.env.WEBSITE_DOMAIN + ':80',
+      'http://' + process.env.WEBSITE_DOMAIN + ':3000',
+      'http://' + process.env.WEBSITE_DOMAIN + ':3001',
+      'https://' + process.env.WEBSITE_DOMAIN,
+      'https://' + process.env.WEBSITE_DOMAIN + ':80',
+      'https://' + process.env.WEBSITE_DOMAIN + ':3000',
+      'https://' + process.env.WEBSITE_DOMAIN + ':3001',
+    ],
+    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
+    credentials: true,
+  });
   console.log(httpsOptions);
   const config = new DocumentBuilder()
     .setTitle('Caroline')
