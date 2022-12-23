408c4a749531c5cd30c76e7f3fa88b582fc6ce70
Sergei Papikian <I_SER_I@hotmail.com>
2022-12-23 15:28:11 +0300

    refactor: remove Cors

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import supertokens from 'supertokens-node';
 
 async function start() {
   const port = process.env.PORT || 3001;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({
-    origin: [
-      process.env.AUTH_WEBSITE_DOMAIN,
-      'http://' + process.env.WEBSITE_DOMAIN,
-      'http://' + process.env.WEBSITE_DOMAIN + ':80',
-      'http://' + process.env.WEBSITE_DOMAIN + ':3000',
-      'http://' + process.env.WEBSITE_DOMAIN + ':3001',
-      'https://' + process.env.WEBSITE_DOMAIN,
-      'https://' + process.env.WEBSITE_DOMAIN + ':80',
-      'https://' + process.env.WEBSITE_DOMAIN + ':3000',
-      'https://' + process.env.WEBSITE_DOMAIN + ':3001',
-    ],
-    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     process.env.AUTH_WEBSITE_DOMAIN,
+  //     'http://' + process.env.WEBSITE_DOMAIN,
+  //     'http://' + process.env.WEBSITE_DOMAIN + ':80',
+  //     'http://' + process.env.WEBSITE_DOMAIN + ':3000',
+  //     'http://' + process.env.WEBSITE_DOMAIN + ':3001',
+  //     'https://' + process.env.WEBSITE_DOMAIN,
+  //     'https://' + process.env.WEBSITE_DOMAIN + ':80',
+  //     'https://' + process.env.WEBSITE_DOMAIN + ':3000',
+  //     'https://' + process.env.WEBSITE_DOMAIN + ':3001',
+  //   ],
+  //   allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
+  //   credentials: true,
+  // });
   const config = new DocumentBuilder()
     .setTitle('Caroline')
     .setVersion('1.0.0')
