741e886f53b1c59475eddc77b68dc51d6b61b5e1
Sergei Papikian <I_SER_I@hotmail.com>
2022-12-23 17:21:12 +0300

    feat: remove CORs

--- src/main.ts
+++ src/main.ts
@@ -2,35 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import supertokens from 'supertokens-node';
-import * as fs from 'fs';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function start() {
   const port = process.env.PORT || 3001;
   const httpsOptions = {
-    key: fs.readFileSync('./secrets/private-key.pem'),
-    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+    key: '',
+    cert: '',
   };
+
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
     new FastifyAdapter({ https: httpsOptions }),
   );
-  app.enableCors({
-    origin: [
-      process.env.AUTH_WEBSITE_DOMAIN,
-      'http://' + process.env.WEBSITE_DOMAIN,
-      'http://' + process.env.WEBSITE_DOMAIN + ':80',
-      'http://' + process.env.WEBSITE_DOMAIN + ':3000',
-      'http://' + process.env.WEBSITE_DOMAIN + ':3001',
-      'https://' + process.env.WEBSITE_DOMAIN,
-      'https://' + process.env.WEBSITE_DOMAIN + ':80',
-      'https://' + process.env.WEBSITE_DOMAIN + ':3000',
-      'https://' + process.env.WEBSITE_DOMAIN + ':3001',
-    ],
-    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     process.env.AUTH_WEBSITE_DOMAIN,
+  //     'http://' + process.env.WEBSITE_DOMAIN,
+  //     'http://' + process.env.WEBSITE_DOMAIN + ':80',
+  //     'http://' + process.env.WEBSITE_DOMAIN + ':3000',
+  //     'http://' + process.env.WEBSITE_DOMAIN + ':3001',
+  //     'https://' + process.env.WEBSITE_DOMAIN,
+  //     'https://' + process.env.WEBSITE_DOMAIN + ':80',
+  //     'https://' + process.env.WEBSITE_DOMAIN + ':3000',
+  //     'https://' + process.env.WEBSITE_DOMAIN + ':3001',
+  //   ],
+  //   allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
+  //   credentials: true,
+  // });
   const config = new DocumentBuilder()
     .setTitle('Caroline')
     .setVersion('1.0.0')
