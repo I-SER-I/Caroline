f709d899aaa34dfc3613a3adbab968701751608b
Sergei Papikian <I_SER_I@hotmail.com>
2022-12-26 01:28:57 +0300

    refactor: Cors policy

--- src/main.ts
+++ src/main.ts
@@ -18,33 +18,42 @@ async function start() {
     AppModule,
     new FastifyAdapter({ https: httpsOptions }),
   );
+
+  const whitelist = [
+    'http://localhost:3000',
+    'imcaroline.me',
+    'http://imcaroline.me',
+    'http://imcaroline.me:3000',
+    'http://imcaroline.me:80',
+    'http://imcaroline.me:443',
+    'https://imcaroline.me',
+    'https://imcaroline.me:3000',
+    'https://imcaroline.me:80',
+    'https://imcaroline.me:443',
+    'http://158.160.44.208',
+    'http://158.160.44.208:3000',
+    'http://158.160.44.208:80',
+    'http://158.160.44.208:443',
+    'https://158.160.44.208',
+    'https://158.160.44.208:3000',
+    'https://158.160.44.208:80',
+    'https://158.160.44.208:443',
+  ];
   app.enableCors({
-    origin: [
-      'imcaroline.me',
-      'http://imcaroline.me',
-      'http://imcaroline.me:3000',
-      'http://imcaroline.me:80',
-      'http://imcaroline.me:443',
-      'https://imcaroline.me',
-      'https://imcaroline.me:3000',
-      'https://imcaroline.me:80',
-      'https://imcaroline.me:443',
-      'http://158.160.44.208',
-      'http://158.160.44.208:3000',
-      'http://158.160.44.208:80',
-      'http://158.160.44.208:443',
-      'https://158.160.44.208',
-      'https://158.160.44.208:3000',
-      'https://158.160.44.208:80',
-      'https://158.160.44.208:443',
-    ],
+    origin: (origin, callback) => {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Accept',
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
       ...supertokens.getAllCORSHeaders(),
     ],
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
   });
   const config = new DocumentBuilder()
